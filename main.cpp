b799b837d1a8d898c21ece96ca314383f4af60bb
L_vv_n <134967146+LevShv@users.noreply.github.com>
2025-03-16 14:30:53 +0300

    Test#1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QQmlApplicationEngine>
 
 #include <MainWindowView.h>
-
+//
 int main(int argc, char *argv[])
 {
 #if defined(Q_OS_WIN) && QT_VERSION_CHECK(5, 6, 0) <= QT_VERSION && QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
