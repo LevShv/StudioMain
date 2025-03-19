b678694d8bf4666ea62615fa499f73c78a5da219
L_vv_n <134967146+LevShv@users.noreply.github.com>
2025-03-19 22:05:53 +0300

    Заготовка под кнопку паузы и включения

--- ViewModel/ViewModel.cpp
+++ ViewModel/ViewModel.cpp
@@ -1,6 +1,22 @@
 #include "ViewModel.h"
+#include <QtCore/QDebug>
 
-ViewModel::ViewModel(QObject* parent)
+ViewModel::ViewModel(QObject* parent) : QObject(parent), m_isPlaying(false)
 {
 
+}
+
+Q_INVOKABLE void ViewModel::togglePlayback()
+{
+	m_isPlaying = !m_isPlaying; // Переключение состояния
+
+	if(m_isPlaying) qDebug() << "Track is playing: la la la o o o  ";
+	else qDebug() << "Track is stopped";
+
+	emit isPlayingChanged(); // Уведомление об изменении состояния
+}
+
+bool ViewModel::isPlaying() const
+{
+	return m_isPlaying;
 }
--- ViewModel/ViewModel.h
+++ ViewModel/ViewModel.h
@@ -1,19 +1,22 @@
 #pragma once
 
 #include <QObject>
-#include <QStringList>
 #include <MainWindowView.h>
 
 
 class ViewModel : public QObject {
-
-private:
-
-	Q_OBJECT
-	//Q_PROPERTY(QStringList tracks READ tracks NOTIFY tracksChanged)
+    Q_OBJECT
+        Q_PROPERTY(bool isPlaying READ isPlaying NOTIFY isPlayingChanged) 
 
 public:
+    explicit ViewModel(QObject* parent = nullptr);
 
-	explicit ViewModel(QObject* parent = nullptr);
+    Q_INVOKABLE void togglePlayback(); // Метод для переключения состояния
+    bool isPlaying() const; // Метод для чтения свойства
 
+signals:
+    void isPlayingChanged(); // Сигнал для уведомления об изменении свойства
+
+private:
+    bool m_isPlaying; // Флаг, указывающий, играет ли трек
 };
