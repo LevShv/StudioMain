150509e1583b7878f263d68f490d82a895435273
L_vv_n <134967146+LevShv@users.noreply.github.com>
2025-06-11 15:20:13 +0300

    Вынужденный

--- ViewModel/ViewModel.h
+++ ViewModel/ViewModel.h
@@ -38,6 +38,10 @@ public:
     Q_INVOKABLE void addMidiTrack();
     Q_INVOKABLE void addSamplerTrack();
 
+    Q_INVOKABLE void changeTrackGain();
+    Q_INVOKABLE void toggleMuteTrack();
+  //  Q_INVOKABLE void toggle
+
     Q_INVOKABLE void moveClip(size_t trackIdx, size_t clipIdx, double newStartTime);
     Q_INVOKABLE void deleteClip(int trackIndex, int clipindex);
     Q_INVOKABLE void deleteClips(const QVariantList& clips); // Новый метод
